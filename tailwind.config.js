efd83a66ed0c47fb82d7887481aba9f607a5581d
musharofahmed1 <musharofahmed11@gmail.com>
2023-07-27 10:51:42 -0600

    edited nav bar again

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,37 @@ module.exports = {
     },
     plugins: [],
   }
+
+  /*
+  <!-- <nav class= "container mx-auto">
+            <div class="navbar bg-orange-400 justify-around h-24 rounded-full">
+              <!--Buttons-->
+              
+              <div class="inline-flex h-11 w-86 list-none text-[#FFF] gap-6 text-xl">
+                   
+                <li><a href="">Home </a></li>
+                <li><a href="">About Us </a></li>
+                <li><a href="">Shop </a></li>
+                <li><a href="">All Fruits </a></li>
+                
+        </div>
+       
+       
+        <!--Logo and Shop Name-->
+        <div class="w-56 h-15 flex flex-row items-center">
+            <img class="w-11 h-15" src="./images/logo.png">
+          <h1 class="py-2 px-2 text-3xl font-extrabold h-12 w-46 text-[#FFF]">Fruit Burst</h1>
+        </div>
+        <!--Search icon-->
+        <div class="flex">
+          <div class="form-control">
+            
+              <input type="text" placeholder="Search"  class="input rounded-3xl w-24 md:w-80 lg:w-100"/>
+              <!--<i class="fa-solid fa-magnifying-glass"></i>-->
+              
+          </div> 
+        </div>
+
+      </div> 
+     </nav>-->
+     */
